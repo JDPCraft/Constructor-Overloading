a4e68b6fae7f048a9a3bb1b601e6b582a938bec7
Jacob Post <jacob.post.2020@student.lhschools.org>
2018-08-29 14:19:25 -0400

    First code in project.
    Time works, defaults to 23 hours 59 minutes if hour is greater than 23 and if minutes are greater than 59. defaults to 0 hours 0 minutes if hour and minutes are less than 0. Adds a 0 before minutes if minutes are less than 10 (Because that's how clock time works.)

--- src/ClassOverloading.java
+++ src/ClassOverloading.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class ClassOverloading {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a time. (Separate the hour and minutes with a space)");
+        TimeOfDay now=new TimeOfDay(keyboard.nextInt(),keyboard.nextInt());
+
+        if(now.getM_Minutes() == 10) {
+            System.out.println(now.getM_Hour() + ":" + now.getM_Minutes());
+        }
+        if(now.getM_Minutes() > 10) {
+            System.out.println(now.getM_Hour() + ":" + now.getM_Minutes());
+        }
+        if(now.getM_Minutes() < 10) {
+            System.out.println(now.getM_Hour() + ":0" + now.getM_Minutes());
+        }
+    }
+
 }
--- src/TimeOfDay.java
+++ src/TimeOfDay.java
@@ -1,2 +1,39 @@
 public class TimeOfDay {
+
+    private int m_Hour;
+    private int m_Minutes;
+
+    public TimeOfDay() {
+        this( 0, 0);
+    }
+
+    public TimeOfDay(int p_Hour) {
+        this(p_Hour, 0);
+    }
+
+    public TimeOfDay(int p_Hour, int p_Minutes) {
+        if (p_Hour > 23) {
+            p_Hour = 23;
+        }
+        if (p_Minutes > 59) {
+            p_Minutes = 59;
+        }
+        if (p_Hour < 0) {
+            p_Hour = 0;
+        }
+        if (p_Minutes < 0) {
+            p_Minutes = 0;
+        }
+
+        this.m_Hour = p_Hour;
+        this.m_Minutes = p_Minutes;
+    }
+
+    public int getM_Hour() {
+        return m_Hour;
+    }
+
+    public int getM_Minutes() {
+        return m_Minutes;
+    }
 }
