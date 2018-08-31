f91b01e47c86c0f676546d90ee57db59d1a5679f
Jacob Post <jacob.post.2020@student.lhschools.org>
2018-08-31 13:28:20 -0400

    Continued code
    Continue and 'finish' on Tuesday, fix errors in code.

--- src/ClassOverloading.java
+++ src/ClassOverloading.java
@@ -1,21 +1,39 @@
 import java.util.Scanner;
 
 public class ClassOverloading {
-
+    private TimeOfDay now;
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter a time. (Separate the hour and minutes with a space)");
-        TimeOfDay now=new TimeOfDay(keyboard.nextInt(),keyboard.nextInt());
+        ClassOverloading classOverloading = new ClassOverloading();
 
-        if(now.getM_Minutes() == 10) {
-            System.out.println(now.getM_Hour() + ":" + now.getM_Minutes());
-        }
-        if(now.getM_Minutes() > 10) {
-            System.out.println(now.getM_Hour() + ":" + now.getM_Minutes());
-        }
-        if(now.getM_Minutes() < 10) {
-            System.out.println(now.getM_Hour() + ":0" + now.getM_Minutes());
-        }
     }
 
+    public void init () {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a time. (Separate the hour and minutes with a space)");
+        TimeOfDay now = new TimeOfDay(keyboard.nextInt(), keyboard.nextInt());
+
+        if (now.getM_Minutes() == 10) {
+            System.out.println(now.getM_Hour() + ":" + now.getM_Minutes());
+        }
+        if (now.getM_Minutes() > 10) {
+            System.out.println(now.getM_Hour() + ":" + now.getM_Minutes());
+        }
+        if (now.getM_Minutes() < 10) {
+            System.out.println(now.getM_Hour() + ":0" + now.getM_Minutes());
+        }
+
+    }
+    public TimeOfDay setTimeOfDay(Scanner pKeyboard, TimeOfDay now) {
+        boolean valid = false;
+        while (!valid) {
+            System.out.println("Enter the desired hour.");
+            int pHour =
+
+        }
+        return now;
+    }
+
+    private boolean setHours(Scanner pKeyboard) {
+
+    }
 }
--- src/TimeOfDay.java
+++ src/TimeOfDay.java
@@ -4,7 +4,7 @@ public class TimeOfDay {
     private int m_Minutes;
 
     public TimeOfDay() {
-        this( 0, 0);
+        this(0, 0);
     }
 
     public TimeOfDay(int p_Hour) {
@@ -12,28 +12,48 @@ public class TimeOfDay {
     }
 
     public TimeOfDay(int p_Hour, int p_Minutes) {
-        if (p_Hour > 23) {
-            p_Hour = 23;
+        if (p_Hour > 23 || p_Hour < 0 || p_Minutes > 59 || p_Minutes < 0) {
+            throw new IllegalArgumentException(
+                    "Input given was not valid in terms of real world time: Hour: " + p_Hour + " Minutes: " + p_Minutes);
         }
-        if (p_Minutes > 59) {
-            p_Minutes = 59;
-        }
-        if (p_Hour < 0) {
-            p_Hour = 0;
-        }
-        if (p_Minutes < 0) {
-            p_Minutes = 0;
+//        if () {
+//            p_Minutes = 59;
+//        }
+//        if () {
+//            p_Hour = 0;
+//        }
+//        if () {
+//            p_Minutes = 0;
+//        }
+
+            this.m_Hour = p_Hour;
+            this.m_Minutes = p_Minutes;
         }
 
-        this.m_Hour = p_Hour;
-        this.m_Minutes = p_Minutes;
+        public int getM_Hour () {
+            return m_Hour;
+        }
+
+        public int getM_Minutes () {
+            return m_Minutes;
+        }
+
+    public void setM_Hour(int p_Hour) {
+        if(p_Hour > 23 || p_Hour < 0)
+        throw new IllegalArgumentException("Input given is invalid. Hour given: " + p_Hour);
+            else {
+                    this.m_Hour = p_Hour;
+        }
+
     }
 
-    public int getM_Hour() {
-        return m_Hour;
+    public void setM_Minutes(int p_Minutes) {
+        if(p_Minutes > 59 || p_Minutes < 0)
+            throw new IllegalArgumentException("Input given is invalid. Minutes given: " + p_Minutes);
+        else {
+            this.m_Minutes = p_Minutes;
+        }
+
     }
 
-    public int getM_Minutes() {
-        return m_Minutes;
-    }
 }
