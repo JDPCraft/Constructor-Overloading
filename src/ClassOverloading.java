f43512f6da971bd45c25d821f227619ddabf0320
Jacob Post <jacob.post.2020@student.lhschools.org>
2018-09-04 13:18:33 -0400

    Code finished, but not expanded
    Code works and returns time given and loops back if time given is invalid, but doesn't allow for additional time inputs and comparing them.

--- src/ClassOverloading.java
+++ src/ClassOverloading.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class ClassOverloading {
     private TimeOfDay now;
+
     public static void main(String[] args) {
         ClassOverloading classOverloading = new ClassOverloading();
-
+        classOverloading.init();
     }
 
     public void init () {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter a time. (Separate the hour and minutes with a space)");
-        TimeOfDay now = new TimeOfDay(keyboard.nextInt(), keyboard.nextInt());
+        now = setTimeOfDay(keyboard, now);
 
         if (now.getM_Minutes() == 10) {
             System.out.println(now.getM_Hour() + ":" + now.getM_Minutes());
@@ -27,13 +28,49 @@ public class ClassOverloading {
         boolean valid = false;
         while (!valid) {
             System.out.println("Enter the desired hour.");
-            int pHour =
+            int pHour = pKeyboard.nextInt();
+            System.out.println("Enter the desired minutes.");
+            int pMinutes = pKeyboard.nextInt();
+            if(validHours(pHour)&&validMinutes(pMinutes)) {
+                now = new TimeOfDay(pHour,pMinutes);
+                valid = true;
+                return now;
+            }
+            else {
+                System.out.println("You have entered invalid time:" + pHour + ":" + pMinutes +"\nPlease enter the " +
+                        "desired time again.");
+            }
 
         }
-        return now;
+        return null;
     }
 
-    private boolean setHours(Scanner pKeyboard) {
-
+    private boolean validHours(int pHour) {
+        if(pHour<24&&pHour>0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
+
+    private boolean validMinutes(int pMinute) {
+        if(pMinute<60&&pMinute>=0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+  /*  private boolean setHours(Scanner pKeyboard) {
+
+    } */
 }
